c39b12bf32af25a061096339942fd90a4e29c32d
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2026-06-27 02:07:00 -0400

    fix: Limpieza de archivos en vista de adaptarnos a la rubrica

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.main.id
+# }
+#
+# output "ec2_public_ip" {
+#   value = aws_instance.web.public_ip
+# }
 
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
-}
