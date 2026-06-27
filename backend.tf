eb0d87bc7b1a10213143849f0e3eb49b1d34a997
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2026-06-27 02:10:02 -0400

    feat: Se agregan archivos para uso de backend s3

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket       = "bucket-de-475554724337para-terrafor-aigues-1782536366"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    # dynamodb_table = "GuardadoTerraform"
+    # terraform dice que ya no es necesario dynamodb para bloquear el estado
+    encrypt = true
+  }
+}
