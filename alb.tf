7ea20bc4419f5c9194a9b1cd4f172f8cd599ba8e
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2026-06-27 23:43:52 -0400

    feat: MVP de ALB, WIP

---
+++ alb.tf
@@ -0,0 +1,38 @@
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 10.5.0"
+
+  name    = "${var.project}-ALB"
+  vpc_id  = module.net_connections.vpc_id
+  subnets = module.net_connections.public_subnets
+
+  create_security_group = false
+  security_groups       = [module.security_group_alb.id]
+
+  listeners = {
+    ex-http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix = "pagina"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "ip"
+      healt_check = {
+        path = "/"
+      }
+    }
+  }
+
+  tags = {
+    Project   = var.project
+    Terraform = "true"
+  }
+}
--- outputs.tf
+++
@@ -1,8 +0,0 @@
-# output "vpc_id" {
-#   value = aws_vpc.main.id
-# }
-#
-# output "ec2_public_ip" {
-#   value = aws_instance.web.public_ip
-# }
-
