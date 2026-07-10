59cbceb23e0d605db9df0760057f4a23b2369bd6
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2026-07-09 21:00:31 -0400

    fix: Solucion de conflictos menor

--- sitio-web/script.js
+++ sitio-web/script.js
@@ -1,71 +1,88 @@
 const form = document.getElementById("bookingForm");
+const mensaje = document.getElementById("mensaje");
+const btn = form.querySelector(".btn");
 
 form.addEventListener("submit", async function(event) {
-
     event.preventDefault();
-
+    btn.disabled = true;
+    btn.textContent = "Enviando...";
+    mensaje.textContent = "";
     const datos = {
         nombre: document.getElementById("nombre").value,
         email: document.getElementById("email").value,
         tramo: document.getElementById("tramo").value,
-        fecha: document.getElementById("fecha").value
+        fecha: document.getElementById("fecha").value,
     };
-
-
-    const respuesta = await fetch("URL_DE_TU_LAMBDA", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(datos)
-    });
-
-
-    const resultado = await respuesta.json();
-
-    console.log(resultado);
-
-});
-
-// let comentarios = []; //ahora se llenará con lambda, 
-
-const comentarios = [
-
-    {
-        nombre:"Camila",
-        fecha:"09/07/2026",
-        texto:"Muy buen recorrido.",
-        foto:"https://i.pravatar.cc/150?img=32"
-    },
-
-    {
-        nombre:"Felipe",
-        fecha:"09/07/2026",
-        texto:"Lo mejor fue la cárcel. asdskj ajks jsa kjak jsdja kssjajk sdladwalijdnalwd wadn dakjsdj asjdkjaskj akjs djajska djkawj",
-        foto:"https://i.pravatar.cc/150?img=12"
-    },
-
-    {
-        nombre:"Javiera",
-        fecha:"09/07/2026",
-        texto:"Volvería nuevamente.",
-        foto:"https://i.pravatar.cc/150?img=48"
-    },
-
-    {
-        nombre:"Ignacio",
-        fecha:"09/07/2026",
-        texto:"Muy distinto a un tour normal.",
-        foto:"https://i.pravatar.cc/150?img=51"
-    },
-
-    {
-        nombre:"Valentina",
-        fecha:"09/07/2026",
-        texto:"Excelente experiencia.",
-        foto:"https://i.pravatar.cc/150?img=15"
+    try {
+        const respuesta = await fetch(
+            "https://i6zid2olkgztgzm34ibksuofom0psqfk.lambda-url.us-east-1.on.aws/",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(datos),
+            },
+        );
+        const resultado = await respuesta.json();
+        if (respuesta.ok) {
+            mensaje.style.color = "green";
+            mensaje.textContent = "Reserva confirmada, revisa tu email";
+            form.reset();
+        } else {
+            mensaje.style.color = "red";
+            mensaje.textContent = "Error, algo salio mal";
+        }
+    } catch (err) {
+        mensaje.style.color = "red";
+        mensaje.textContent = " Error de conexion";
+    } finally {
+        btn.disabled = false;
+        btn.textContent = "Reservar";
     }
 
+    const resultado = await respuesta.json();
+    console.log(resultado);
+});
+
+// let comentarios = []; //ahora se llenará con lambda,
+
+const comentarios = [
+    {
+        nombre: "Camila",
+        fecha: "09/07/2026",
+        texto: "Muy buen recorrido.",
+        foto: "https://i.pravatar.cc/150?img=32",
+    },
+
+    {
+        nombre: "Felipe",
+        fecha: "09/07/2026",
+        texto:
+            "Lo mejor fue la cárcel. asdskj ajks jsa kjak jsdja kssjajk sdladwalijdnalwd wadn dakjsdj asjdkjaskj akjs djajska djkawj",
+        foto: "https://i.pravatar.cc/150?img=12",
+    },
+
+    {
+        nombre: "Javiera",
+        fecha: "09/07/2026",
+        texto: "Volvería nuevamente.",
+        foto: "https://i.pravatar.cc/150?img=48",
+    },
+
+    {
+        nombre: "Ignacio",
+        fecha: "09/07/2026",
+        texto: "Muy distinto a un tour normal.",
+        foto: "https://i.pravatar.cc/150?img=51",
+    },
+
+    {
+        nombre: "Valentina",
+        fecha: "09/07/2026",
+        texto: "Excelente experiencia.",
+        foto: "https://i.pravatar.cc/150?img=15",
+    },
 ];
 
 let actual = 0;
@@ -74,8 +91,7 @@ const izquierda = document.querySelector(".testimonio-left");
 const centro = document.querySelector(".testimonio-center");
 const derecha = document.querySelector(".testimonio-right");
 
-function cargar(card, dato){
-
+function cargar(card, dato) {
     card.innerHTML = `
 
         <img src="${dato.foto}" alt="${dato.nombre}">
@@ -91,44 +107,34 @@ function cargar(card, dato){
         <p>${dato.texto}</p>
 
     `;
-
 }
 
-function actualizar(){
-
+function actualizar() {
     const anterior = (actual - 1 + comentarios.length) % comentarios.length;
     const siguiente = (actual + 1) % comentarios.length;
 
     cargar(izquierda, comentarios[anterior]);
     cargar(centro, comentarios[actual]);
     cargar(derecha, comentarios[siguiente]);
-
 }
 
 actualizar();
 
 document.getElementById("testimonio-next").addEventListener("click", () => {
-
     actual = (actual + 1) % comentarios.length;
 
     actualizar();
-
 });
 
 document.getElementById("testimonio-prev").addEventListener("click", () => {
-
     actual = (actual - 1 + comentarios.length) % comentarios.length;
 
     actualizar();
-
 });
 
 const botonTestimonio = document.getElementById("mostrar-formulario");
 const formularioTestimonio = document.getElementById("formulario-testimonio");
 
-
 botonTestimonio.addEventListener("click", () => {
-
     formularioTestimonio.classList.toggle("oculto");
-
 });
